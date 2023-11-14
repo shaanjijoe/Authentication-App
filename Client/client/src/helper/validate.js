d9eaeb9c9f81d728964c5d3d39f48359bf18c27f
Harman <harmansaluja02@gmail.com>
2023-11-14 17:53:39 +0530

    Reset UI Recovery UI Password UI and Username UI completed

--- Client/client/src/helper/validate.js
+++ Client/client/src/helper/validate.js
@@ -1,9 +1,45 @@
 import toast from 'react-hot-toast';
-
+// validate username
 export async function usernameValidate(values){
     const errors = usernameVerify({},values)
     return errors;
 }
+
+export async function passwordValidate(values){
+    const errors = passwordVerify({},values)
+    return errors;
+}
+export async function resetpasswordValidate(values){
+    const errors = passwordVerify({},values)
+    if(values.password!==values.confirm_password)
+    errors.exist = toast.error("Password doesn't match");
+    return errors;
+}
+
+// validate password
+function passwordVerify(errors={},values)
+{
+    const specialchars= /[!@#$%^&*()_+[\]{};':"\\|,.<>/?]+/;
+    if(!values.password)
+    {
+        errors.password = toast.error("Password Required...!");
+    }
+    else if(values.password.includes(" "))
+    {
+        errors.password = toast.error("Wrong Password...!");
+    }
+    else if(values.password.length<4)
+    {
+        errors.password = toast.error("Password is weak");
+    }
+    else if(!specialchars.test(values.password)){
+        errors.password = toast.error("Password must have special chars");
+    }
+    return errors;
+}
+
+
+
 
 //Here we used toast to complete the task
 
